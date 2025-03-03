87f9ce3de55815be5b4069f0c641804d411ad17a
MikeBulenga <jerome.boulanger.mail@gmail.com>
2025-03-03 16:48:40 +0100

    Create button.js

---
+++ src/components/ui/button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button({ children, onClick }) {
+  return (
+    <button className="p-2 bg-blue-500 text-white rounded" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
